0011892f669f06e87d0484cc5a6004a21e3b45e1
AitorSantaeugenia <aitor.j.s.m@gmail.com>
2021-09-30 04:41:35 +0200

    done some models

---
+++ models/User.model.js
@@ -0,0 +1,16 @@
+const mongoose = require('mongoose');
+const { Schema, model } = mongoose;
+
+const userSchema = new Schema({
+	email: { type: String, unique: true, required: true },
+	password: { type: String, required: true },
+	name: { type: String, required: true },
+	surname: String,
+	image: String,
+	dateCreated: {
+		type: Date,
+		default: Date.now
+	}
+});
+
+module.exports = model('User', userSchema);
