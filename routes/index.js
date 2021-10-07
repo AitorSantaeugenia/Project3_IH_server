3d7f15159d7d0ae9032a1e6c193acffd5f3d344d
AitorSantaeugenia <aitor.j.s.m@gmail.com>
2021-10-07 14:38:27 +0200

    change

--- routes/index.js
+++ routes/index.js
@@ -1,15 +1,15 @@
 const router = require('express').Router();
-const Hotel = require('../models/Hotel.model');
+// const Hotel = require('../models/Hotel.model');
 const multer = require('./../config/cloudinary-config');
 
 router.get('/', (req, res, next) => {
-	Hotel.find()
-		.then((hotel) => {
-			res.json(hotel);
-			//console.log(hotel);
-		})
-		.catch((err) => res.json(err));
-	//res.json('All good in here');
+	// Hotel.find()
+	// 	.then((hotel) => {
+	// 		res.json(hotel);
+	// 		//console.log(hotel);
+	// 	})
+	// 	.catch((err) => res.json(err));
+	res.json('All good in here');
 });
 
 router.post('/upload', multer.single('file'), (req, res) => {
